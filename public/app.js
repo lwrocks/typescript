bc9284a40e97d57b43aae0ddf192faacf31c24cd
Laurel Wright <wright.laureld@gmail.com>
2022-09-18 20:01:01 +0200

    First commit 🎉

---
+++ public/app.js
@@ -0,0 +1,26 @@
+"use strict";
+// Classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes £${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice("Mario", "Work on the Mario website", 250);
+const invTwo = new Invoice("Luigi", "Work on the Luigi website", 300);
+let invoices = [];
+const form = document.querySelector(".new-item-form");
+// console.log(form.children);
+// Inputs
+const type = document.querySelector("#type");
+const toFrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
---
+++ src/app.ts
@@ -0,0 +1,36 @@
+// Classes
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+
+  format() {
+    return `${this.client} owes £${this.amount} for ${this.details}`;
+  }
+}
+
+const invOne = new Invoice("Mario", "Work on the Mario website", 250);
+const invTwo = new Invoice("Luigi", "Work on the Luigi website", 300);
+
+let invoices: Invoice[] = [];
+
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+// console.log(form.children);
+
+// Inputs
+const type = document.querySelector("#type") as HTMLSelectElement;
+const toFrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
